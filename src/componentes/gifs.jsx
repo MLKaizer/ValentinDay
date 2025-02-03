4eb956e53659a6c7ef91e91b7d76e4e4ef3429b2
Marcus <okram1008@gmail.com>
2025-02-03 14:45:50 -0600

    todo lsito

--- src/componentes/gifs.jsx
+++ src/componentes/gifs.jsx
@@ -30,7 +30,7 @@ function Gifs() {
       </header>
 
       <section className="item-gif">
-        <img src="public\ositos.gif" alt="Gif animado" />
+        <img src="./ositos.gif" alt="Gif animado" />
       </section>
 
       <footer className="botones">
